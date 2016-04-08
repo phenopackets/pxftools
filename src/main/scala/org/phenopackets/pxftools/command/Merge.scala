0bb8bf310d0188d5a590da44e37a86f7bbcdd095
Jim Balhoff <jim@balhoff.org>
2016-04-08 11:16:37 -0400

    Simplification.

--- src/main/scala/org/phenopackets/pxftools/command/Merge.scala
+++ src/main/scala/org/phenopackets/pxftools/command/Merge.scala
@@ -1,22 +1,9 @@
 package org.phenopackets.pxftools.command
 
-import java.io.BufferedWriter
-import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
 import java.io.File
-import java.io.FileInputStream
-import java.io.FileOutputStream
-import java.io.IOException
-import java.io.InputStream
-import java.io.OutputStream
-import java.io.OutputStreamWriter
-import org.apache.commons.io.IOUtils
+
 import org.backuity.clist._
 import org.phenopackets.api.PhenoPacket
-import org.phenopackets.api.io.JsonGenerator
-import org.phenopackets.api.io.JsonReader
-import org.phenopackets.api.io.YamlGenerator
-import org.phenopackets.api.io.YamlReader
 import org.phenopackets.pxftools.util.MergeUtil
 
 object Merge extends Command(description = "Read in multiple PXF files and output as a single merged PXF file in the specified format.") with Common {
@@ -24,7 +11,7 @@ object Merge extends Command(description = "Read in multiple PXF files and outpu
   var files = args[Seq[File]](description = "List of PXF files to merge")
 
   override def run(): Unit = {
-    val mergedPhenoPacket = files.map(readPhenoPacketFile).foldLeft(new PhenoPacket.Builder().build())(MergeUtil.mergePhenoPackets)
+    val mergedPhenoPacket = files.map(readPhenoPacketFile).reduce(MergeUtil.mergePhenoPackets)
     writePhenoPacket(mergedPhenoPacket, determineOutput, outputWriter)
   }
 
