e759f734f4c7d857afabb25fbeaed6b5fe9c5fd8
Jeremy Nguyen Xuan <jnguyenxuan@lbl.gov>
2016-05-19 22:33:02 -0700

    added merge unit tests

--- src/test/scala/org/phenopackets/pxftools/MergeUtilTest.scala
+++ src/test/scala/org/phenopackets/pxftools/MergeUtilTest.scala
@@ -11,6 +11,10 @@ import org.specs2.runner.JUnitRunner
 import org.junit.runner.RunWith
 import org.specs2.ScalaCheck
 import org.phenopackets.pxftools.PhenoPacketGenerator
+import org.phenopackets.pxftools.PhenoPackeCollectiontMatchers._
+import scala.collection.JavaConversions._
+import org.phenopackets.api.model.association.DiseaseOccurrenceAssociation
+import java.util.{ List => JList }
 
 @RunWith(classOf[JUnitRunner])
 class MergeUtilTest extends Specification with ScalaCheck {
@@ -24,6 +28,14 @@ class MergeUtilTest extends Specification with ScalaCheck {
       val mergedPacket = MergeUtil.mergePhenoPackets(packet1, packet2)
 
       mergedPacket.getTitle must beEqualTo(packet2.getTitle)
+      mergedPacket.getDiseaseOccurrenceAssociations.toList must beTheSameDiseaseOccurrenceAssociationList(packet1.getDiseaseOccurrenceAssociations.toList ::: packet2.getDiseaseOccurrenceAssociations.toList)
+      mergedPacket.getDiseases.toList must beTheSameDiseaseList(packet1.getDiseases.toList ::: packet2.getDiseases.toList)
+      mergedPacket.getEnvironmentAssociations.toList must beTheSameEnvironmentAssociationList(packet1.getEnvironmentAssociations.toList ::: packet2.getEnvironmentAssociations.toList)
+      mergedPacket.getOrganisms.toList must beTheSameOrganismList(packet1.getOrganisms.toList ::: packet2.getOrganisms.toList)
+      mergedPacket.getPersons.toList must beTheSamePersonList(packet1.getPersons.toList ::: packet2.getPersons.toList)
+      mergedPacket.getPhenotypeAssociations.toList must beTheSamePhenotypeAssociationList(packet1.getPhenotypeAssociations.toList ::: packet2.getPhenotypeAssociations.toList)
+      mergedPacket.getVariantAssociations.toList must beTheSameVariantAssociationList(packet1.getVariantAssociations.toList ::: packet2.getVariantAssociations.toList)
+      mergedPacket.getVariants.toList must beTheSameVariantList(packet1.getVariants.toList ::: packet2.getVariants.toList)
     }
 
   }
---
+++ src/test/scala/org/phenopackets/pxftools/PhenoPacketMatchers.scala
@@ -0,0 +1,49 @@
+package org.phenopackets.pxftools
+
+import java.util.{ List => JList }
+import org.phenopackets.api.PhenoPacket
+import org.specs2.matcher._
+import org.phenopackets.api.model.association.DiseaseOccurrenceAssociation
+import org.phenopackets.api.model.condition.DiseaseOccurrence
+import org.phenopackets.api.model.evidence.Evidence
+import org.phenopackets.api.model.condition.DiseaseStage
+import org.phenopackets.api.model.evidence.Publication
+import org.phenopackets.api.model.entity.Disease
+import org.phenopackets.api.model.association.EnvironmentAssociation
+import org.phenopackets.api.model.entity.Organism
+import org.phenopackets.api.model.entity.Person
+import org.phenopackets.api.model.association.PhenotypeAssociation
+import org.phenopackets.api.model.association.VariantAssociation
+import org.phenopackets.api.model.entity.Variant
+
+trait PhenoPackeCollectiontMatchers extends MustMatchers {
+  import scala.collection.JavaConverters._
+  def beTheSameDiseaseOccurrenceAssociationList(list1: List[DiseaseOccurrenceAssociation]): Matcher[List[DiseaseOccurrenceAssociation]] =
+    ((list2: List[DiseaseOccurrenceAssociation]) => forallWhen(list1 zip list2) { case (l1, l2) => l1 must beEqualTo(l2) })
+
+  def beTheSameEvidenceList(list1: scala.collection.immutable.List[Evidence]): Matcher[scala.collection.immutable.List[Evidence]] =
+    ((list2: List[Evidence]) => forallWhen(list1 zip list2) { case (l1, l2) => l1 must beEqualTo(l2) })
+
+  def beTheSameDiseaseList(list1: List[Disease]): Matcher[List[Disease]] =
+    ((list2: List[Disease]) => forallWhen(list1 zip list2) { case (l1, l2) => l1 must beEqualTo(l2) })
+
+  def beTheSameEnvironmentAssociationList(list1: List[EnvironmentAssociation]): Matcher[List[EnvironmentAssociation]] =
+    ((list2: List[EnvironmentAssociation]) => forallWhen(list1 zip list2) { case (l1, l2) => l1 must beEqualTo(l2) })
+
+  def beTheSameOrganismList(list1: List[Organism]): Matcher[List[Organism]] =
+    ((list2: List[Organism]) => forallWhen(list1 zip list2) { case (l1, l2) => l1 must beEqualTo(l2) })
+
+  def beTheSamePersonList(list1: List[Person]): Matcher[List[Person]] =
+    ((list2: List[Person]) => forallWhen(list1 zip list2) { case (l1, l2) => l1 must beEqualTo(l2) })
+
+  def beTheSamePhenotypeAssociationList(list1: List[PhenotypeAssociation]): Matcher[List[PhenotypeAssociation]] =
+    ((list2: List[PhenotypeAssociation]) => forallWhen(list1 zip list2) { case (l1, l2) => l1 must beEqualTo(l2) })
+
+  def beTheSameVariantAssociationList(list1: List[VariantAssociation]): Matcher[List[VariantAssociation]] =
+    ((list2: List[VariantAssociation]) => forallWhen(list1 zip list2) { case (l1, l2) => l1 must beEqualTo(l2) })
+
+  def beTheSameVariantList(list1: List[Variant]): Matcher[List[Variant]] =
+    ((list2: List[Variant]) => forallWhen(list1 zip list2) { case (l1, l2) => l1 must beEqualTo(l2) })
+}
+
+object PhenoPackeCollectiontMatchers extends PhenoPackeCollectiontMatchers
